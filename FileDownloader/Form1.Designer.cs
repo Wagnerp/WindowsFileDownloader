6d08d949d122a6847f0f7bf983e78874eb37cb2e
Gurinder Hans <hello@gurinderhans.me>
2014-11-25 16:04:56 -0800

    initial commit - working for only .pdf files

---
+++ FileDownloader/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace FileDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.url = new System.Windows.Forms.TextBox();
+            this.download_files = new System.Windows.Forms.Button();
+            this.urlsListBox = new System.Windows.Forms.ListBox();
+            this.downloadProgress = new System.Windows.Forms.ProgressBar();
+            this.currentUrlDownloading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // url
+            // 
+            this.url.Location = new System.Drawing.Point(12, 12);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(168, 20);
+            this.url.TabIndex = 0;
+            // 
+            // download_files
+            // 
+            this.download_files.Location = new System.Drawing.Point(197, 9);
+            this.download_files.Name = "download_files";
+            this.download_files.Size = new System.Drawing.Size(75, 23);
+            this.download_files.TabIndex = 1;
+            this.download_files.Text = "Download";
+            this.download_files.UseVisualStyleBackColor = true;
+            this.download_files.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.urlsListBox.FormattingEnabled = true;
+            this.urlsListBox.Location = new System.Drawing.Point(12, 38);
+            this.urlsListBox.Name = "listBox1";
+            this.urlsListBox.Size = new System.Drawing.Size(516, 290);
+            this.urlsListBox.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.downloadProgress.Location = new System.Drawing.Point(12, 383);
+            this.downloadProgress.Name = "progressBar1";
+            this.downloadProgress.Size = new System.Drawing.Size(516, 23);
+            this.downloadProgress.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.currentUrlDownloading.AutoSize = true;
+            this.currentUrlDownloading.Location = new System.Drawing.Point(12, 353);
+            this.currentUrlDownloading.Name = "label1";
+            this.currentUrlDownloading.Size = new System.Drawing.Size(35, 13);
+            this.currentUrlDownloading.TabIndex = 4;
+            this.currentUrlDownloading.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 430);
+            this.Controls.Add(this.currentUrlDownloading);
+            this.Controls.Add(this.downloadProgress);
+            this.Controls.Add(this.urlsListBox);
+            this.Controls.Add(this.download_files);
+            this.Controls.Add(this.url);
+            this.Name = "Form1";
+            this.Text = "File Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.Button download_files;
+        private System.Windows.Forms.ListBox urlsListBox;
+        private System.Windows.Forms.ProgressBar downloadProgress;
+        private System.Windows.Forms.Label currentUrlDownloading;
+    }
+}
+
---
+++ FileDownloader/Form1.cs
@@ -0,0 +1,91 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Queue<string> allDownloadUrls = new Queue<string>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String downloadUrl = url.Text; //downloadUrl = "http://www.cs.sfu.ca/CourseCentral/101.MACM/abulatov";
+
+            //parse the html
+            HtmlWeb hw = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc = hw.Load(downloadUrl);
+
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                String web_url = link.Attributes["href"].Value;
+                if (web_url.Contains("pdf"))
+                {
+                    urlsListBox.Items.Add(web_url);//add to done box
+                    allDownloadUrls.Enqueue(web_url);//add all urls to queue
+                }
+            }
+            //images in progress
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//img[@src]"))
+            {
+                String web_url = link.Attributes["src"].Value;
+                urlsListBox.Items.Add(web_url);
+                if (web_url.Contains("pdf"))
+                {
+                    //urlsListBox.Items.Add(web_url);//add to done box
+                    //allDownloadUrls.Enqueue(web_url);//add all urls to queue
+                }
+            }
+            DownloadFile();
+        }
+
+        private void DownloadFile()
+        {
+            if (allDownloadUrls.Any())
+            {
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+
+                var thisUrl = allDownloadUrls.Dequeue();
+                String fileName = Path.GetFileName(thisUrl);
+
+                currentUrlDownloading.Text = "Downloading: "+thisUrl;
+
+                // Starts the download
+                client.DownloadFileAsync(new Uri(thisUrl), @"C:\Users\ghans\Documents\Visual Studio 2013\Projects\FileDownloader\FileDownloader\bin\Debug\downloads\"+fileName+"");
+            }
+            // End of the download
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+
+            downloadProgress.Value = int.Parse(Math.Truncate(percentage).ToString());
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            //start the next
+            DownloadFile();
+        }
+    }
+}
